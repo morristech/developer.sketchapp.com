06e4b5d97dfddfafafa430d1d0631fd899b4ce59
Sam Deane <sam@elegantchaos.com>
2016-05-09 12:53:00 +0100

    added docs rebuild

--- example-plugins/rebuild.sh
+++ example-plugins/rebuild.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
-base=`dirname $0`
-cd "$base/.."
+scripts=`dirname $0`
+cd "$scripts/.."
+base=`pwd`
 
 examples=( "plugins.actions.selected" )
 
 for e in $examples
 do
-  dest="example-plugins/plugins/$e"
+  dest="$base/example-plugins/plugins/$e"
   if [[ ! -e "$dest" ]]
   then
     git clone "git@github.com:BohemianCoding/$e" "$dest"
+    cd "$dest"
+  else
+    cd "$dest"
+    git pull
   fi
+
+  docco --output "$base/example-plugins/docs/$e" *.sketchplugin/Contents/Sketch/*.js
 done
