dd957658de781d7ab05344e06b939a2a5fa840ad
Sam Deane <sam@elegantchaos.com>
2016-05-09 12:40:04 +0100

    updated rebuild script

--- example-plugins/rebuild.sh
+++ example-plugins/rebuild.sh
@@ -3,4 +3,13 @@
 base=`dirname $0`
 cd "$base/.."
 
-git clone git@github.com:BohemianCoding/plugins.actions.selected example-plugins/plugins/selected
+examples=( "plugins.actions.selected" )
+
+for e in $examples
+do
+  dest="example-plugins/plugins/$e"
+  if [[ ! -e "$dest" ]]
+  then
+    git clone "git@github.com:BohemianCoding/$e" "$dest"
+  fi
+done
