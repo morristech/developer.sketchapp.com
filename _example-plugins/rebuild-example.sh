0698424507be510a3d94980f3269e7d29c321493
Sam Deane <sam@elegantchaos.com>
2016-05-12 15:26:25 +0100

    added zipping of plugins

--- _example-plugins/rebuild-example.sh
+++ _example-plugins/rebuild-example.sh
@@ -2,10 +2,17 @@
 
 dest="$1"
 scripts="$2"
+name=`basename *.sketchplugin .sketchplugin`
 
 echo "Updating $dest"
 git pull
 
 echo "Regenerating $dest"
 docout="$scripts/../examples/plugins"
-docco --output "$docout" --template "$scripts/docco.jst" --css "$scripts/docco.css" *.sketchplugin/Contents/Sketch/*.js
+docco --output temp --template "$scripts/docco.jst" --css "$scripts/docco.css" *.sketchplugin/Contents/Sketch/*.js
+name=`basename temp/*.html .html`
+mv -f "temp/$name.html" "$scripts/../examples/plugins/"
+rm -rf temp
+
+echo "Making zip"
+ditto -ck *.sketchplugin "$scripts/../examples/plugins/$name.zip"
