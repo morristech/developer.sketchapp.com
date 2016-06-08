d958a4bc01298d3d7d32b641ca00b1a7b2be7d39
Sam Deane <sam@elegantchaos.com>
2016-06-08 10:31:38 +0100

    added a jsdoc config file, and disabled docco for API documentation

--- _example-plugins/build-api-docs.sh
+++ _example-plugins/build-api-docs.sh
@@ -8,5 +8,14 @@ fullname=$(basename "$dest")
 name="${fullname##*.}"
 
 rm -rf "$base/_api"
-docco --layout linear --output "$base/_api" --template "$scripts/linear.jst" --css "$scripts/linear.css" "$base/../Sketch/Modules/SketchPluginManager/Javascript/Source/"*.js
-rm "$base/_api/linear.css"
+
+# Build using Docco
+#docco --layout linear --output "$base/_api" --template "$scripts/linear.jst" --css "$scripts/linear.css" "$base/../Sketch/Modules/SketchPluginManager/Javascript/Source/"*.js
+#rm "$base/_api/linear.css"
+
+# Build using jsdoc
+cd "$base/../Sketch/Modules/SketchPluginManager/Javascript"
+"node_modules/.bin/jsdoc" --configure "$scripts/jsdoc.json" --destination "$base/_api" --verbose Source/*.js
+
+# Build using headerdoc
+#"headerdoc2html" -o "$base/_api" "$base/../Sketch/Modules/SketchPluginManager/Javascript/Source/"*.js
