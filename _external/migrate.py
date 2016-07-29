4bbe0a59ed247ae187b1e800bf53d547b58b635f
Sam Deane <sam@elegantchaos.com>
2016-07-29 17:19:29 +0100

    Extract description and shortcut from comment

--- _external/migrate.py
+++ _external/migrate.py
@@ -9,6 +9,7 @@ import sys
 import os
 import shutil
 import json
+import re
 
 def check_arguments():
     if len(sys.argv) != 2:
@@ -23,20 +24,7 @@ def read_plugin(plugin):
         return text
 
 
-def convert_plugin(plugin, source):
-    print "Converting {0}".format(plugin)
-
-    (path, nameAndExt) = os.path.split(plugin)
-    (name, ext) = os.path.splitext(nameAndExt)
-    converted = os.path.join(path, "{0}-converted{1}".format(name, ext))
-
-    if os.path.exists(converted):
-        shutil.rmtree(converted)
-
-    sketchFolderPath = os.path.join(converted, 'Content','Sketch')
-    os.makedirs(sketchFolderPath)
-
-    description = ""
+def build_manifest(name, source):
     author = os.getenv('USER')
     company = "{0}s-plugins".format(author)
     email = "{0}@{1}.com".format(author, company)
@@ -45,7 +33,7 @@ def convert_plugin(plugin, source):
 
     manifest = {
         "name": name,
-        "description": description,
+        "description": "",
         "author": author,
         "authorEmail": email,
         "version": "2.0",
@@ -66,6 +54,31 @@ def convert_plugin(plugin, source):
         }],
     }
 
+    pattern = re.compile('^//\W*(.*)\W* (\((.*)\))*?$', re.MULTILINE)
+    match = pattern.match(source)
+    if match:
+        manifest['description'] = match.group(1)
+        shortcut = match.group(3)
+        if shortcut:
+            manifest['shortcut'] = shortcut
+
+    return manifest
+
+
+def convert_plugin(plugin, source):
+    print "Converting {0}".format(plugin)
+
+    (path, nameAndExt) = os.path.split(plugin)
+    (name, ext) = os.path.splitext(nameAndExt)
+    converted = os.path.join(path, "{0}-converted{1}".format(name, ext))
+
+    if os.path.exists(converted):
+        shutil.rmtree(converted)
+
+    sketchFolderPath = os.path.join(converted, 'Content','Sketch')
+    os.makedirs(sketchFolderPath)
+
+    manifest = build_manifest(name, source)
     with open(os.path.join(sketchFolderPath, 'manifest.json'), 'w') as file:
         file.write(json.dumps(manifest, indent = 2))
 
