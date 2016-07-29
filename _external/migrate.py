a310e6d375c7aa8f1d324e371db963fecb567266
Sam Deane <sam@elegantchaos.com>
2016-07-29 12:07:45 +0100

    start of implementation

--- _external/migrate.py
+++ _external/migrate.py
@@ -6,5 +6,41 @@
 # --------------------------------------------------------------------------
 
 import sys
+import os
+import shutil
 
-print sys.argv[1]
+def check_arguments():
+    if len(sys.argv) != 2:
+        print "Usage: {0} <plugin-path>.".format(os.path.basename(sys.argv[0]))
+        exit(1)
+
+
+
+def read_plugin(plugin):
+    with open(plugin, 'r') as file:
+        text = file.read()
+        return text
+
+
+def convert_plugin(plugin):
+    print "Converting {0}".format(plugin)
+
+    (path, nameAndExt) = os.path.split(plugin)
+    (name, ext) = os.path.splitext(nameAndExt)
+    converted = os.path.join(path, "{0}-converted{1}".format(name, ext))
+
+    if os.path.exists(converted):
+        shutil.rmtree(converted)
+
+    os.makedirs(os.path.join(converted, 'Content','Sketch'))
+
+
+def main():
+    check_arguments()
+    path = sys.argv[1]
+    source = read_plugin(path)
+    print source
+    convert_plugin(path)
+
+
+main()
