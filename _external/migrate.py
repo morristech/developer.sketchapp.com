9f96e81421393d03880bb97f488f35915b76794a
Sam Deane <sam@elegantchaos.com>
2016-07-29 17:04:39 +0100

    write out a manifest

--- _external/migrate.py
+++ _external/migrate.py
@@ -8,6 +8,7 @@
 import sys
 import os
 import shutil
+import json
 
 def check_arguments():
     if len(sys.argv) != 2:
@@ -22,7 +23,7 @@ def read_plugin(plugin):
         return text
 
 
-def convert_plugin(plugin):
+def convert_plugin(plugin, source):
     print "Converting {0}".format(plugin)
 
     (path, nameAndExt) = os.path.split(plugin)
@@ -32,15 +33,49 @@ def convert_plugin(plugin):
     if os.path.exists(converted):
         shutil.rmtree(converted)
 
-    os.makedirs(os.path.join(converted, 'Content','Sketch'))
+    sketchFolderPath = os.path.join(converted, 'Content','Sketch')
+    os.makedirs(sketchFolderPath)
+
+    description = ""
+    author = os.getenv('USER')
+    company = "{0}s-plugins".format(author)
+    email = "{0}@{1}.com".format(author, company)
+    identifier = "com.{0}.{1}".format(company, name)
+    home = "https:/github.com/{0}/{1}".format(company, name)
+
+    manifest = {
+        "name": name,
+        "description": description,
+        "author": author,
+        "authorEmail": email,
+        "version": "2.0",
+        "identifier": identifier,
+        "homepage": home,
+        "compatibleVersion": 39,
+        "commands": [{
+            "identifier": "svgo",
+            "handler": "___svgo_run_handler_",
+            "script": "plugin.js",
+            "name": "About SVGO Compressor",
+            "handlers": {
+                "run": "___svgo_run_handler_",
+                "actions": {
+                    "ExportSlices": "___svgo_run_handler_"
+                }
+            }
+        }],
+    }
+
+    with open(os.path.join(sketchFolderPath, 'manifest.json'), 'w') as file:
+        file.write(json.dumps(manifest, indent = 2))
+
 
 
 def main():
     check_arguments()
     path = sys.argv[1]
     source = read_plugin(path)
-    print source
-    convert_plugin(path)
+    convert_plugin(path, source)
 
 
 main()
