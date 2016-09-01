be6d020ba8158ef29abdea1ae1cb1bf5cb9ddfc1
Sam Deane <sam@elegantchaos.com>
2016-09-01 16:58:48 +0100

    don't actually need to inject custom css or script file, or insert any html instead we're stripping off the generated <html><head> etc and just keeping what was in the <body> tag

--- _external/api-docs-plugin.js
+++ _external/api-docs-plugin.js
@@ -1,45 +1,16 @@
+
+
 var fs = require("fs");
 
 exports.onStart = function(ev) {
   // take option
-  preheaderPath = ev.data.option["header"];
+  scriptsPath = ev.data.option["scripts"];
 };
-
-// exports.onHandleConfig = function(ev) {
-//   // modify config
-//   ev.data.config.title = ...;
-// };
-//
-// exports.onHandleCode = function(ev) {
-//   // modify code
-//   ev.data.code = ...;
-// };
-//
-// exports.onHandleCodeParser = function(ev) {
-//   // modify parser
-//   ev.data.parser = function(code){ ... };
-// };
-//
-// exports.onHandleAST = function(ev) {
-//   // modify AST
-//   ev.data.ast = ...;
-// };
-//
-// exports.onHandleTag = function(ev) {
-//   // modify tag
-//   ev.data.tag = ...;
-// };
 
 exports.onHandleHTML = function(ev) {
-  // modify HTML
-  // var preheader = fs.readFileSync(preheaderPath).toString();
-  // wrappedPreheader = "<preheader>\n" + preheader + "\n</preheader>\n\n"
+  // extract everything inside the <body> tag and throw the rest away
   match = ev.data.html.match(/body.*>([^]*)<\/body/)
   if (match) {
-    ev.data.html = "---\n\n---\n\n" + match[1];
+    ev.data.html = "---\nlayout: api-page\n---\n\n" + match[1];
   }
 };
-
-// exports.onComplete = function(ev) {
-//   // complete
-// };
--- _external/build-api-docs.sh
+++ _external/build-api-docs.sh
@@ -16,9 +16,7 @@ configpath=/tmp/esdoc.json
 echo "{" > "$configpath"
 echo "\"source\": \"$base/../SketchAPI/Source/\"," >> "$configpath"
 echo " \"destination\": \"$base/reference/api\"," >> "$configpath"
-echo " \"plugins\": [ { \"name\": \"$scripts/api-docs-plugin.js\", \"option\": { \"header\": \"$scripts/api-docs-header.html\" } } ]," >> "$configpath"
-echo " \"styles\": [ \"$scripts/api-docs.css\" ], " >> "$configpath"
-echo " \"scripts\": [ \"$scripts/api-docs.js\" ]" >> "$configpath"
+echo " \"plugins\": [ { \"name\": \"$scripts/api-docs-plugin.js\", \"option\": { \"scripts\": \"$scripts\" } } ]," >> "$configpath"
 echo " }" >> "$configpath"
 
 "$esdoc" -c "$configpath"
