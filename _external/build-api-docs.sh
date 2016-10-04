5d337c4d24efff046216b149b93622aa52c59f95
Sam Deane <sam@elegantchaos.com>
2016-10-04 22:02:49 +0100

    Cleaned up script.

--- _external/build-api-docs.sh
+++ _external/build-api-docs.sh
@@ -7,22 +7,23 @@ base=`pwd`
 fullname=$(basename "$dest")
 name="${fullname##*.}"
 
-src="/tmp/esdoc/api"
-rm -rf "$src" 2> /dev/null
-mkdir -p "$src"
-
-dest="$base/_generated-api"
-rm -rf "$dest" 2> /dev/null
-mkdir -p "$dest"
-
 # Build using esdoc
 esdoc="$(npm bin)/esdoc"
 
-# Copy source files
+# Copy source files into temporary location
+src="/tmp/esdoc/api"
+rm -rf "$src" 2> /dev/null
+mkdir -p "$src"
 cp -r "$base/../SketchAPI/Source/"*.js "$src/"
 cp "$base/../SketchAPI/README.md" "$src/"
 cd "$src/"
 
+# Start with a clean destination
+dest="$base/_generated-api"
+rm -rf "$dest" 2> /dev/null
+mkdir -p "$dest"
+
+# Write out a temporary config file
 configpath="$src/esdoc.json"
 echo "{" > "$configpath"
 echo "\"source\": \"$src\"," >> "$configpath"
@@ -30,13 +31,9 @@ echo " \"destination\": \"$dest\"," >> "$configpath"
 echo " \"plugins\": [ { \"name\": \"$scripts/api-docs-plugin.js\", \"option\": { \"scripts\": \"$scripts\" } } ]" >> "$configpath"
 echo " }" >> "$configpath"
 
-pwd
-ls
 "$esdoc" -c "$configpath"
 
 # Remove the json files, and the raw source files, which we probably don't need
 rm -f "$dest/"*.json
-rm -f "$dest/"*.js
-rm -f "$dest/README.md"
 rm -rf "$dest/ast"
 rm -rf "$src"
