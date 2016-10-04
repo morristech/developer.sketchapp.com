c1cc3eacccd590cbd27733d95bf2aeaf9a7bc4f5
Sam Deane <sam@elegantchaos.com>
2016-10-04 21:57:51 +0100

    Relocated the generated API documentation to _generated-api

--- _external/build-api-docs.sh
+++ _external/build-api-docs.sh
@@ -7,7 +7,11 @@ base=`pwd`
 fullname=$(basename "$dest")
 name="${fullname##*.}"
 
-dest="$base/_generated/api"
+src="/tmp/esdoc/api"
+rm -rf "$src" 2> /dev/null
+mkdir -p "$src"
+
+dest="$base/_generated-api"
 rm -rf "$dest" 2> /dev/null
 mkdir -p "$dest"
 
@@ -15,17 +19,19 @@ mkdir -p "$dest"
 esdoc="$(npm bin)/esdoc"
 
 # Copy source files
-cp -r "$base/../SketchAPI/Source/"*.js "$dest/"
-cp "$base/../SketchAPI/README.md" "$dest/"
-cd "$dest"
+cp -r "$base/../SketchAPI/Source/"*.js "$src/"
+cp "$base/../SketchAPI/README.md" "$src/"
+cd "$src/"
 
-configpath=/tmp/esdoc.json
+configpath="$src/esdoc.json"
 echo "{" > "$configpath"
-echo "\"source\": \"$dest\"," >> "$configpath"
+echo "\"source\": \"$src\"," >> "$configpath"
 echo " \"destination\": \"$dest\"," >> "$configpath"
 echo " \"plugins\": [ { \"name\": \"$scripts/api-docs-plugin.js\", \"option\": { \"scripts\": \"$scripts\" } } ]" >> "$configpath"
 echo " }" >> "$configpath"
 
+pwd
+ls
 "$esdoc" -c "$configpath"
 
 # Remove the json files, and the raw source files, which we probably don't need
@@ -33,3 +39,4 @@ rm -f "$dest/"*.json
 rm -f "$dest/"*.js
 rm -f "$dest/README.md"
 rm -rf "$dest/ast"
+rm -rf "$src"
