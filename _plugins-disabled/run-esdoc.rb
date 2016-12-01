b1f15b9bb06ec9f9c961e971c53c9c8f9b1a6bf4
Ale Muñoz <bomberstudios@gmail.com>
2016-12-01 13:08:27 +0100

    Remove plugins folder to see if that fixes the GH build error

--- _plugins-disabled/run-esdoc.rb
+++
@@ -1,5 +0,0 @@
-Jekyll::Hooks.register :site, :pre_render do |site|
-  system("./_external/build-api-docs.sh")
-  system("./_external/build_plugin-examples.sh")
-  system("echo 'blah' > /tmp/test-blah.txt")
-end
